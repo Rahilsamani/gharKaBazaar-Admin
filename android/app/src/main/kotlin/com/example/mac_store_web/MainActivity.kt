4bf6176b5470f9a03950a929c71139788e33b91d
Rahilsamani <rahilahmed1720@gmail.com>
2024-08-06 23:18:04 +0530

    add initial files

---
+++ android/app/src/main/kotlin/com/example/mac_store_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ghar_ka_bazaar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ install_flutter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter precache
+flutter doctor
